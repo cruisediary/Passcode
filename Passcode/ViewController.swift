4cbeb6864d8dc5256e60514da5b662c079292995
cruz <cruzdiary@gmail.com>
2017-05-18 23:30:33 +0900

    Initial Commit

---
+++ Passcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Passcode
+//
+//  Created by CruzDiary on 18/05/2017.
+//  Copyright © 2017 Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
