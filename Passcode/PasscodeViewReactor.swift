09acb472f0b819e80b82b246cd15bc40c9543273
Cruz <cruzdiary@gmail.com>
2018-09-25 13:57:52 +0900

    Remove custom func shuffle()

--- Passcode/PasscodeViewReactor.swift
+++ Passcode/PasscodeViewReactor.swift
@@ -12,21 +12,6 @@ import ReactorKit
 import RxCocoa
 import RxSwift
 
-extension MutableCollection where Indices.Iterator.Element == Index {
-  /// Shuffles the contents of this collection.
-  mutating func shuffle() {
-    let c = count
-    guard c > 1 else { return }
-    
-    for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-      let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
-      guard d != 0 else { continue }
-      let i = index(firstUnshuffled, offsetBy: d)
-      swap(&self[firstUnshuffled], &self[i])
-    }
-  }
-}
-
 extension Sequence {
   /// Returns an array with the contents of this sequence, shuffled.
   func shuffled() -> [Iterator.Element] {
